fd1885001e3eea832dca3b8ed8c5929558979ac5
Roberto <roberto.skinkis@sk.lvt.lv>
2023-03-29 16:05:29 +0300

    uzsakt darbs ar image objecteik

---
+++ Assets/skripti/VardaParadisana.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour {
+	int kadri = 0;	
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Programma tika starteta");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Kadrs: "+kadri);
+		kadri++;
+		if (kadri==10) {
+			Debug.Log ("Tu sasniedzi 10.kadru");
+	}
+}
+	public string teksts;
+
+	public GameObject ievadesLauks;
+
+	public GameObject tekstaAttelosana;
+	private string[] varianti = { "Sveiki", "Labdien", "DIIIIIIIIIIIIIIIIIIIRSA"};
+	int nejausais;
+	public void NolasitTekstu(){
+		teksts = ievadesLauks.GetComponent<Text>().text;	
+		nejausais = Random.Range (0, varianti.Length);
+		tekstaAttelosana.GetComponent<Text> ().text =
+			varianti [nejausais] + teksts.ToUpper () + "!";
+}
+}
--- README.md
+++ README.md
@@ -3,13 +3,13 @@ unity projekts kas demonstre ui elementu lietojumu par temu Mr. beanst
 ![MR.Beanst attels](https://user-images.githubusercontent.com/128358109/228151247-e9e8629f-aec4-4f34-a854-7f94e61a0ad5.png)
 
 ## Daramo darbu saraksts
-- [ ] Input field objekta litojums projekta
-- [ ] Text objekta lietojums projekta
-- [ ] button objects lietojums projekta
+- [x] Input field objekta litojums projekta
+- [x] Text objekta lietojums projekta
+- [x] button objects lietojums projekta
 - [ ] image objekta lietojums projekta
 - [ ] toggle objekta lietojums projekta
 - [ ] toggle group objekta lietojums projekta
-- [ ] C#
+- [x] C#
 - [ ] drop down objekta lietojums projekta
 - [ ] Slider objekta lietojums projekta
 - [ ] audio source objekta lietojums projekta
